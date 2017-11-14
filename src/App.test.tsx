5ea87340d144e8b40472a263c4d5519b1bb4aad9
olomonos <o.lomonos@gmail.com>
2017-11-14 22:22:19 +0200

    initial commit;

---
+++ src/App.test.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<div />, div);
+});
---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import registerServiceWorker from './registerServiceWorker';
+import './index.css';
+
+ReactDOM.render(
+  <div />,
+  document.getElementById('root') as HTMLElement
+);
+registerServiceWorker();
