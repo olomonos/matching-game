39bf7312f78a593d300b432de61de87e39a46242
olomonos <o.lomonos@gmail.com>
2017-11-17 19:23:25 +0200

    function set-of-random-coords added;

--- src/store/reducers/current-circles.tsx
+++ src/store/reducers/current-circles.tsx
@@ -1,13 +1,8 @@
 import {Reducer} from './reducer';
 import {Store} from '../types';
-import {randomCoords} from '../../utils/random-coords';
+import {setOfRandomCoords} from '../../utils/set-of-random-coords';
 
-const initialCircles: Store['currentCircles'] = [
-    randomCoords(300, 600, 50, 50, 2),
-    randomCoords(300, 600, 50, 50, 2),
-    randomCoords(300, 600, 50, 50, 2),
-    randomCoords(300, 600, 50, 50, 2)    
-];
+const initialCircles: Store['currentCircles'] = setOfRandomCoords(4);
 
 export const currentCircles: Reducer<Store['currentCircles']> = (
     state = initialCircles, action
---
+++ src/utils/set-of-random-coords.ts
@@ -0,0 +1,11 @@
+import {randomCoords} from './random-coords';
+import {Coords} from '../store';
+
+export const setOfRandomCoords = function (quantity: number) : Coords[] {
+    let set: Coords[] = [];
+    for (let i = 0; i < quantity; i++) {
+        let coords = randomCoords(300, 600, 50, 50, 2);
+        set.push(coords);
+    }
+    return set;
+};
