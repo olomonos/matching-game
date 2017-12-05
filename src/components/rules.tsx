66c8ae7e163052d5df665bf3a8b55a4bb564e53c
olomonos <o.lomonos@gmail.com>
2017-12-05 16:43:10 +0200

    rules component added;

---
+++ src/components/rules.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+export const Rules: React.StatelessComponent = () => {
+
+    return (
+        <div className='rules-box'>
+            <span>You should find the odd circle in the right part of the field.</span>
+        </div>
+    )
+};
--- src/layouts/game.tsx
+++ src/layouts/game.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
+import {Rules} from '../components/rules';
 import Field from '../containers/field';
 import GameMenu from '../containers/game-menu';
 
 export const Game: React.StatelessComponent = () => (
     <div>
         <GameMenu className='game-menu'/>
+        <Rules />
         <Field />
     </div>
 );
